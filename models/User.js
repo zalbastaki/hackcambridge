4efa5057eaf17f8d4b0f4d04abae10ede1d9e863
Zain Albastaki <zain.albastaki@gmail.com>
2019-01-20 10:47:12 +0000

    finished profile info section

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ User.add({
 	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
-	avatar: { type: Types.CloudinaryImage },
+	avatar: { type: Types.CloudinaryImage, default: null },
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
 });
